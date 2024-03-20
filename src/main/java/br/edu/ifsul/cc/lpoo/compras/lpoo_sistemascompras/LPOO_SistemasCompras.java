3de127f2f1c17a8e9aa363e619b45a9940c2fda5
nicolasscopel <nicolaspscopel@gmail.com>
2024-03-20 11:02:33 -0300

    teste

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/compras/lpoo_sistemascompras/LPOO_SistemasCompras.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.cc.lpoo.compras.lpoo_sistemascompras;
+
+/**
+ *
+ * @author 20222PF.CC0019
+ */
+public class LPOO_SistemasCompras {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/br/edu/ifsul/cc/lpoo/compras/lpoo_sistemascompras/TestPersistencia.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.cc.lpoo.compras.lpoo_sistemascompras;
+
+/**
+ *
+ * @author 20222PF.CC0019
+ */
+public class TestPersistencia {
+    
+}
